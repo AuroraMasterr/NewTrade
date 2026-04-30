89f875f25cec0f3fe87bf9eb33a37665ea184f36
冯中信 <615917284@qq.com>
2026-04-30 22:19:15 +0800

    fix

--- data_purge/download.ps1
+++ data_purge/download.ps1
@@ -1,6 +1,6 @@
 python data_purge/download.py `
     --symbol BTCUSDT `
     --interval "1h" `
-    --start "2025-01" `
-    --end "2026-04" `
+    --start "2025-01-01" `
+    --end "2026-04-29" `
     *> download.log
--- data_purge/download.py
+++ data_purge/download.py
@@ -1,14 +1,21 @@
 import os
 import requests
 import argparse
-from datetime import datetime
+from datetime import datetime, timedelta
 from tqdm import tqdm
 
-# Binance
-BASE_URL = "https://data.binance.vision/data/spot/monthly/klines"
+
+def get_base_url(mode, freq):
+    """
+    Binance Api
+    mode: [spot, futures_um, futures_cm] 现货 / U本位合约 / 币本位合约
+    freq: [monthly, daily]
+    """
+    return f"https://data.binance.vision/data/{mode.replace("_", "/")}/{freq}/klines"
 
 
 def download(url, file):
+    print(f"Download URL: {url} for {file}")
     filename = os.path.basename(file)
     response = requests.get(url, stream=True)
     response.raise_for_status()
@@ -24,20 +31,21 @@ def download(url, file):
             if chunk:
                 f.write(chunk)
                 bar.update(len(chunk))
-    print(f"✓ Completed: {filename}")
+    print(f"[OK] Completed: {filename}")
 
 
-def binance_download(year, month, symbol, interval):
+def binance_download(year, month, day, symbol, interval, mode, freq):
     m = str(month).zfill(2)
-    filename = f"{symbol}-{interval}-{year}-{m}.zip"
+    d = str(day).zfill(2)
+    filename = f"{symbol}-{interval}-{year}-{m}-{d}.zip"
     save_dir = f"raw_data/{symbol}/{interval}"
     os.makedirs(save_dir, exist_ok=True)
     file = os.path.join(save_dir, filename)
     if os.path.exists(file):
         print(f"Skip, Already exists: {filename}")
         return
+    BASE_URL = get_base_url(mode, freq)
     url = f"{BASE_URL}/{symbol}/{interval}/{filename}"
-    print(f"Downloading: {filename}")
     download(url, file)
 
 
@@ -45,26 +53,38 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--symbol", type=str, default="BTCUSDT")
     parser.add_argument("--interval", type=str, required=True)
-    # 起始/结束时间，YYYY-MM
+    # 起始/结束时间，YYYY-MM-DD
     parser.add_argument("--start", type=str, required=True)
     parser.add_argument("--end", type=str, required=True)
+    parser.add_argument(
+        "--mode",
+        type=str,
+        default="futures_um",
+        choices=["spot", "futures_um", "futures_cm"],
+    )
+    parser.add_argument(
+        "--freq",
+        type=str,
+        default="daily",
+        choices=["monthly", "daily"],
+    )
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
-    symbol = args.symbol
-    interval = args.interval
-
-    start_time = datetime.strptime(args.start, "%Y-%m").date()
-    end_time = datetime.strptime(args.end, "%Y-%m").date()
+    start_time = datetime.strptime(args.start, "%Y-%m-%d").date()
+    end_time = datetime.strptime(args.end, "%Y-%m-%d").date()
     if start_time > end_time:
         raise ValueError("start time must be before end time")
-    year, month = start_time.year, start_time.month
-    end_year, end_month = end_time.year, end_time.month
-    while (year, month) <= (end_year, end_month):
-        binance_download(year, month, symbol, interval)
-        month += 1
-        if month > 12:
-            month = 1
-            year += 1
+    while start_time <= end_time:
+        binance_download(
+            start_time.year,
+            start_time.month,
+            start_time.day,
+            args.symbol,
+            args.interval,
+            args.mode,
+            args.freq,
+        )
+        start_time += timedelta(days=1)
